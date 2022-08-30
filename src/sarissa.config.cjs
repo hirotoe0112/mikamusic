23357f6dd758bd1506ffe234b25890dbf448b1d6
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2022-08-30 19:43:31 +0900

    initial commit

---
+++ src/sarissa.config.cjs
@@ -0,0 +1,42 @@
+export default {
+  siteTitle: "mikamusic",
+  siteDescription: "フリー音源配布サイト",
+  favicon: "/favicon.ico",
+  siteImagePath: "/images/sarissa.png",
+  footer: "© 2022 - 2022 Suzuka Mikami",
+  dateFormat: "yyyy/MM/dd",
+  pageSize: 20,
+  categories: [
+    {
+      name: "theme",
+      color: "btn-warning",
+      image: "/images/theme.jpg",
+      order: 1,
+    },
+  ],
+  categorySettings: {
+    order: "name", // name | count
+    layout: "card", //button | card
+    image: "",
+    color: "btn-warning",
+    countVisibility: true,
+  },
+  searchOptions: {
+    includeScore: true,
+    includeMatches: true,
+    keys: [
+      { name: "title", weight: 3 },
+      { name: "description", weight: 2 },
+    ],
+  },
+  i18n: {
+    search: {
+      placeholder: "Search post title and description...",
+    },
+    archive: {
+      select: "Select Year",
+    },
+    page: "Page",
+    resultFound: " result(s) found",
+  },
+};
